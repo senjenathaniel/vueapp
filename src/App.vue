2f4661b4d6e6f6000db8cb787a8d6555f40bc409
senjenathaniel <topsailcashew@icloud.com>
2022-06-23 21:09:52 +0300

    tutorial done

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,25 @@
 <template>
-  <div v-if="products.length">
-    <ul>
-      <li v-for="product in products" v-bind:key="product.id">
-        {{ product.title }} - {{ [product.price] }}
-      </li>
-    </ul>
-  </div>
-  <div v-else>
-    <p>No data to display</p>
-  </div>
+  <HeaderItem @changeTitle="changeTitle" v-bind:TextHeader="text" />
+  <p>{{ name }}</p>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
+import HeaderItem from "./components/Header.vue";
 export default {
   name: "App",
   data() {
     return {
-      products: [
-        { id: 1, title: "Apple Watch", price: 50000 },
-        { id: 2, title: "Apple MacBook Pro", price: 12000 },
-        { id: 3, title: "Apple iMac", price: 25000 }
-      ],
-    };
+      text: "Text from App component"
+    }
+  },
+  components: {
+    HeaderItem,
+  },
+
+  methods: {
+    changeTitle(NewTitle) {
+      this.text = NewTitle;
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>{{ TextHeader }}</h2>
+  <button v-on:click="changeText">Change Title</button>
+</template>
+
+<script>
+export default {
+  name: "HeaderItem",
+  data() {
+    return {
+      text: "Text from Headers component",
+      name: "Nathaniel from Header"
+    }
+  },
+  methods: {
+    changeText() {
+      this.$emit("changeTitle", this.text);
+    }
+  },
+
+  emits: ["changeTitle"],
+
+  props: ["TextHeader"]
+}
+</script>
